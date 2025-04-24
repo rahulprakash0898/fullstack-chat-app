dc1aa6d6dea2892f7ac66e82d4996412f21e7f40
Rahul Prakash <rahulprakash0898@gmail.com>
2025-04-24 14:24:03 +0530

    add dist

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://fullstack-chat-app-5ane.onrender.com/api",
   withCredentials: true,
 });
