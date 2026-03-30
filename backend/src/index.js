de1124d1bc4d8e73be671f86baade34c43b36de7
Rahul Prakash <rahulprakash0898@gmail.com>
2026-03-30 17:31:38 +0530

    Cors Policy Fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,20 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://frontend-chat-app-hovj.onrender.com"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
@@ -39,5 +50,3 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
-
-
