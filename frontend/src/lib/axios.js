1343f62dc624ffc19f795a0e1d93994ddf95d16f
Rahul Prakash <rahulprakash0898@gmail.com>
2026-03-31 17:25:17 +0530

    Cookies Problem Solved

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://fullstack-chat-app-5ane.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
 
