d003747c3ddc9a961766bf111778756e157f9347
Rahul Prakash <rahulprakash0898@gmail.com>
2026-03-31 17:17:19 +0530

    Databse Env Variable Updated

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGODB_URI);
-    const conn = await mongoose.connect(process.env.MONGODB_COMPASS);
+    const uri = process.env.MONGODB_URI || process.env.MONGODB_COMPASS;
+    const conn = await mongoose.connect(uri);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
