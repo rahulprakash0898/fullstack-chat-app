b279279c54cbc3d6911d8e40f511570799753328
Rahul Prakash <rahulprakash0898@gmail.com>
2026-08-17 16:26:07 +0530

    Configure app for both local and live production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,13 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +24,6 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.onrender.com",
     ],
     credentials: true,
   })
@@ -31,10 +33,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
